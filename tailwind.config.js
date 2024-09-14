7fdd4fae8e14d9ab4cfa0d7f7631ffbf767994ca
Kouji Takao <kouji.takao@gmail.com>
2024-09-14 08:29:02 +0000

    fix: style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './config/initializers/simple_form.rb'
   ],
   theme: {
     fontFamily: {
