ed507634ad564c35c1d133fff6bffa72717fccc0
Kouji Takao <kouji.takao@gmail.com>
2024-09-14 09:00:09 +0000

    fix: sort order

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first)
+    @quote = Quote.last
   end
 
   test "Creating a new quote" do
